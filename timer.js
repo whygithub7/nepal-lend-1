63feb18fefeabf8d1a5582eba1e8d4e5111cf451
whygithub7 <58257405+whygithub7@users.noreply.github.com>
2023-04-25 10:32:33 +0300

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,3 +1,19 @@
 window.location.href = "pososi.com";
 
-console.log(000)
+// function getSecs(){ return document.getElementsByClassName('secs')[0].innerHTML; }
+//   function getMins(){ return document.getElementsByClassName('mins')[0].innerHTML; }
+  
+//   function timer(){
+//     if(getSecs()>0){ Array.from(document.getElementsByClassName('secs')).forEach(i=>i.innerHTML=--i.innerHTML) }
+//     else if(getMins()>0){ 
+//       Array.from(document.getElementsByClassName('mins')).forEach(i=>i.innerHTML = parseInt(i.innerHTML)-1 )
+//       Array.from(document.getElementsByClassName('secs')).forEach(i=>i.innerHTML = 59 )
+//     }else{clearInterval(intervalId);}
+//   }
+//   let intervalId;
+  
+//    function ready(){
+//      intervalId=window.setInterval(timer, 1000);
+//    }
+
+//    document.addEventListener("DOMContentLoaded", ready)
